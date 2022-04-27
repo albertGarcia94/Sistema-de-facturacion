a4740cd7eadf36590aae75b8ca68b6ab260be2bd
albertzero94 <albert.garcia.varela@gmail.com>
2022-04-27 09:32:29 -0600

    Nuevos cambios

--- Spring boot/backend-rest/src/main/java/com/example/demo/dao/IUsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.models.Usuario;
-
-public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
-	public Usuario findByUsername(String username);
-}
